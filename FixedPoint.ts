601a6b96023e31750588eceab2736811154c902e
Ted Avtar <theted@berkeley.edu>
2023-07-17 12:20:34 -0700

    More untested functions, sprinkle in |0's

--- FixedPoint.ts
+++ FixedPoint.ts
@@ -4,21 +4,29 @@ export class FixedPoint {
 	// the scaling factor is 1/(2^16) or 2^-16
 	static scaleShift = 16;
 	
-	// This is the unscaled, backing number that is an integer
+	// This is the unscaled, backing number that is a signed integer
 	// This number should be able to be precisely represented in 32 bit 2's complement
 	// We divide this by 2^16 to get the value this object represents
 	rawValue: number;
 
 	constructor(rawValue: number) {
-		this.rawValue = rawValue;
+		this.rawValue = rawValue | 0;
 	}
 
 	static fromNumber(n: number): FixedPoint {
 		return new FixedPoint((n << FixedPoint.scaleShift) | 0);
 	}
 
+	static toNumber(a: FixedPoint): number {
+		return a.rawValue/(1 << FixedPoint.scaleShift);
+	}
+
 	static add(a: FixedPoint, b: FixedPoint): FixedPoint {
-		return new FixedPoint(a.rawValue + b.rawValue);
+		return new FixedPoint(((a.rawValue|0) + (b.rawValue|0))|0);
+	}
+
+	static sub(a: FixedPoint, b: FixedPoint): FixedPoint {
+		return new FixedPoint(((a.rawValue|0) - (b.rawValue|0))|0);
 	}
 
 
